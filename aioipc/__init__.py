9c2c957595da07abcc3c85e5519e79cd5904e164
Riks Designer <riksdesigner@gmail.com>
2021-02-27 17:45:20 +0100

    Version changes

--- aioipc/__init__.py
+++ aioipc/__init__.py
@@ -1,10 +1,5 @@
-from collections import namedtuple
-
 from aioipc.client import Client
 from aioipc.server import Server
 from aioipc.errors import *
 
-__version__ = "1.0.2"
-
-_VersionInfo = namedtuple("_VersionInfo", "major minor micro releaselevel serial")
-version_info = _VersionInfo(major=1, minor=0, micro=2, releaselevel="final", serial=0)
+__version__ = '1.0.0-a'
--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
 import re
 import setuptools
 
-with open("aioipc/__init__.py") as stream:
-    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", stream.read(), re.MULTILINE).group(1)
+with open("aioipc/__init__.py") as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
 
 setuptools.setup(
     author="Riksou",
